24e40285a670d3c6aa45d6705f946a77c80f9b39
hwantae <kwon99814@naver.com>
2025-04-26 08:02:05 +0000

    first setting

---
+++ src/main/java/com/example/skkuTable/SkkuTableApplication.java
@@ -0,0 +1,13 @@
+package com.example.skkuTable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkkuTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkkuTableApplication.class, args);
+	}
+
+}
